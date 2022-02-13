24dbc96c990222c9d52fb5cae327e7ddb2386621
hojiakbar <mansuroff145@gmail.com>
2022-02-13 13:47:27 +0500

    contact

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class AuthorModel(models.Model):
         return '{} {}'.format(self.first_name, self.last_name)
 
     def __str__(self):
-        return self.first_name
+        return self.full_name
 
     class Meta:
         verbose_name = 'author'
@@ -32,8 +32,17 @@ class BlogPostModel(models.Model):
     description = models.TextField(verbose_name=_('description'))
     main_img = models.ImageField(upload_to='blog-post-main-img/', verbose_name=_('main image'))
     banner = models.ImageField(upload_to='blog-post-main-img/', verbose_name=_('banner'))
-    author = models.ForeignKey(AuthorModel, on_delete=models.RESTRICT, related_name='posts', verbose_name=_('author'))
-    tags = models.ManyToManyField(BlogTagModel, related_name='posts', verbose_name=_('tags'))
+    author = models.ForeignKey(
+        AuthorModel,
+        on_delete=models.RESTRICT,
+        related_name='posts',
+        verbose_name=_('author')
+    )
+    tags = models.ManyToManyField(
+        BlogTagModel,
+        related_name='posts',
+        verbose_name=_('tags')
+    )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
 
     def __str__(self):
@@ -42,3 +51,23 @@ class BlogPostModel(models.Model):
     class Meta:
         verbose_name = 'blog'
         verbose_name_plural = 'blogs'
+
+class CommentModel(models.Model):
+    post = models.ForeignKey(
+        BlogPostModel,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name=_('post')
+    )
+    name = models.CharField(max_length=64, verbose_name=_('name'))
+    email = models.EmailField(verbose_name=_('email'))
+    phone = models.CharField(max_length=13, verbose_name=_('phone'))
+    comment = models.TextField(verbose_name=_('comment'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created at'))
+
+    class Meta:
+        verbose_name = 'comment'
+        verbose_name_plural = 'comments'
+
+    def __str__(self):
+        return self.name
